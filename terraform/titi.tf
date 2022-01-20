a7cfb907c025d3efec1f9c17e2d7a1b222ef1869
Patrick Fortier <patrick.fortier@lacapitale.com>
2022-01-20 15:58:39 +0000

    add 3 accounts

---
+++ terraform/titi.tf
@@ -0,0 +1,28 @@
+module "titi-account" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "aws.acc.lab+titi-lacapitale20012022@beneva.ca"
+    AccountName               = "titi"
+    ManagedOrganizationalUnit = "AFT"
+    SSOUserEmail              = "aws.acc.lab+ssouser1-lacapitale20012022@beneva.ca"
+    SSOUserFirstName          = "ssouser1"
+    SSOUserLastName           = "ssouser1"
+  }
+
+  account_tags = {
+    "ENV" = "labo"
+    "CC"  = "12345"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "titi"
+}
