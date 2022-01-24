ae93dd5d52c899b1f21aa0f652f31c93e93f50db
Patrick Fortier <patrick.fortier@lacapitale.com>
2022-01-24 21:43:56 +0000

    OU test2

--- terraform/toto.tf
+++ terraform/toto.tf
@@ -4,7 +4,7 @@ module "toto-account" {
   control_tower_parameters = {
     AccountEmail              = "aws.acc.lab+toto-lacapitale20012022@beneva.ca"
     AccountName               = "toto"
-    ManagedOrganizationalUnit = "test0"
+    ManagedOrganizationalUnit = "ou-hqs3-85sc7rti"
     SSOUserEmail              = "aws.acc.lab+ssouser1-lacapitale20012022@beneva.ca"
     SSOUserFirstName          = "ssouser1"
     SSOUserLastName           = "ssouser1"
