4020373806d7daf9ba7f7f0015b1eb3aec66bfa5
Patrick Fortier <patrick.fortier@lacapitale.com>
2022-01-25 21:03:10 +0000

    adfd ou id

--- terraform/tete.tf
+++ terraform/tete.tf
@@ -4,7 +4,7 @@ module "tete-account" {
   control_tower_parameters = {
     AccountEmail              = "aws.acc.lab+tete-lacapitale20012022@beneva.ca"
     AccountName               = "tete"
-    ManagedOrganizationalUnit = "test2"
+    ManagedOrganizationalUnit = "test2(ou-hqs3-85sc7rti)"
     SSOUserEmail              = "aws.acc.lab+ssouser1-lacapitale20012022@beneva.ca"
     SSOUserFirstName          = "ssouser1"
     SSOUserLastName           = "ssouser1"
